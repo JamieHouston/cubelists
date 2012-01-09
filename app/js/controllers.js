c1ba497601bb39c8f17991d29dd9f34884adf2d2
foresterh <foresterh@gmail.com>
2012-01-08 21:18:06 -0800

    setting up persistence

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -26,3 +26,84 @@ function ListDetails(List){
 
 function ConfigController() {
 }
+
+function TodoController (persistencejs) {
+    var self = this;
+    self.newTodo = "";
+	self.editTodoStartContent = "";
+
+    self.addTodo = function() {
+        if (self.newTodo.length === 0) return;
+        
+        self.todos.push({
+            content: self.newTodo,
+            done: false,
+            editing: false
+        });
+		persistencejs.add(self.newTodo);
+        self.newTodo = "";
+    };
+
+    self.editTodo = function(todo) {
+        angular.forEach(self.todos, function(value) {
+            value.editing = false;
+        });
+        todo.editing = true;
+		self.editTodoStartContent = todo.content;
+    };
+
+	self.changeStatus = function(todo){
+		persistencejs.changeStatus(todo);
+	};
+	
+    self.finishEditing = function(todo) {
+        todo.editing = false;
+		persistencejs.edit(self.editTodoStartContent, todo.content);
+    };
+
+    self.removeTodo = function(todo) {
+        angular.Array.remove(self.todos, todo);
+		persistencejs.remove(todo);
+    };
+
+    self.todos = [];
+
+    var countTodos = function(done) {
+        return function() {
+            return angular.Array.count(self.todos, function(x) {
+                return x.done === (done === "done");
+            });
+        }
+    };
+
+    self.remainingTodos = countTodos("undone");
+
+    self.finishedTodos = countTodos("done");
+
+    self.clearCompletedItems = function() {
+        var oldTodos = self.todos;
+        self.todos = [];
+        angular.forEach(oldTodos, function(todo) {
+            if (!todo.done) self.todos.push(todo);
+        });
+		persistencejs.clearCompletedItems();
+    };
+
+    self.hasFinishedTodos = function() {
+        return self.finishedTodos() > 0;
+    };
+
+    self.hasTodos = function() {
+        return self.todos.length > 0;
+    };
+	
+	self.loadTodos = function(){
+		persistencejs.fetchAll(self);
+	}
+	
+	self.refresh = function(){ self.$apply(); }
+
+	self.loadTodos();
+};
+
+TodoController.$inject = ['persistencejs'];
--- app/js/services.js
+++ app/js/services.js
@@ -9,12 +9,63 @@ angular.service('List', function($resource) {
  });
 });
 
-angular.services('Connect', function($resource){
-  var id = 0;
-  $xhr('GET', 'http://localhost:8002/join', { nick: 'taglist' }, function(code, response) {
-    //handle the server's response to our nickname and join request
-    id   = response.id;
+angular.service('persistencejs', function() {
+  persistence.store.websql.config(persistence, 'taglist', 'todo database', 5*1024*1024);
+  var Todo = persistence.define('todo', {
+    content: 'TEXT',
+    done: 'BOOL'
   });
+  persistence.schemaSync();
+  return {
+    add: function(item){
+      var t = new Todo();
+      t.content = item;
+      t.done = false;
+      persistence.add(t);
+      persistence.flush();
+    },
+    
+    edit: function(startContent, endContent){
+      Todo.all().filter('content','=',startContent).one(function(item){
+        item.content = endContent;
+        persistence.flush();
+      });
+    },
+    
+    changeStatus: function(item){
+      Todo.all().filter('content','=',item.content).one(function(todo){
+        todo.done = item.done;
+        persistence.flush();
+      });
+    },
+    
+    clearCompletedItems: function(){
+      Todo.all().filter('done','=',true).destroyAll();
+    },
+    
+    remove: function(item){
+      Todo.all().filter('content','=',item.content).destroyAll();
+    },
+    
+    fetchAll: function(controller){
+      Todo.all().list(function(items){
+        var itemCount = items.length;
+        var todos = [];
+        items.forEach(function(item){
+          todos.push({
+            content: item.content,
+            done: item.done,
+            editing: false
+          });
+          if(--itemCount == 0){
+            controller.todos = todos;
+            controller.refresh();
+          }
+        });
+      });
+    },
+  };
+});
 
 
 angular.service('myAngularApp', function($route, $location, $window) {
